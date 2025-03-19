8dd9fe0d1ed595acc71cf4befd0ebd7d1f83fbbe
hildebrt <t1manster@gmail.com>
2025-03-19 19:44:54 -0400

    Revised template and project structure

--- template.py
+++ template.py
@@ -7,7 +7,6 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 project_name = 'wasteDetection'
 
 list_of_files = [
-    '.github/workflows/.gitkeep',
     'data/.gitkeep',
     f'{project_name}/__init__.py',
     f'{project_name}/components/__init__.py',
