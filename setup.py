b257ec973c9233faede12914361a835db3dbd92d
hildebrt <t1manster@gmail.com>
2025-03-19 19:23:05 -0400

    Created the following: template for folder/file structure, requirements, and setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'CV_Waste_Detection',
+    version= '0.0.0',
+    author= 'Tom Hildebrand',
+    author_email= 't1manster@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
